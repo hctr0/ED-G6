63f3c72153f904826d8421a7da9b2195181a7114
Héctor <hahju2010@gmail.com>
2021-04-25 09:57:21 -0500

    Se agregan comentarios a ListNodes
    Comentarios generales sobre el comportamiento de las funciones para mejorar comprensión

--- src/resources/ListNodes.py
+++ src/resources/ListNodes.py
@@ -1,11 +1,17 @@
 from resources.Nodo import *
+
+
 class ListNodes:
+
+    #CONSTRUCTOR DE CLASE
     def __init__(self,Nodo):
         self.Nodo = Nodo
         self.tam =1
 
+
+    #AGREGA UN NODO Y AUMENTA EN 1 SU TAMAÑO
     def agregarNodo(self,data, nodo2 = None):
-        node =Nodo(data)
+        node = Nodo(data)
         if(self.Nodo.siguiente == None):
             self.Nodo.siguiente = node
             self.tam +=1
@@ -19,13 +25,22 @@ class ListNodes:
                     self.agregarNodo(data,nodo2.siguiente)    
             else:
                 self.agregarNodo(data,self.Nodo.siguiente)    
+    
+
+    #DEVUELVE EL ÚLTIMO NODO DE FORMA RECURSIVA
     def retornarUltimoNodo(self,nodo2 ):
         if(nodo2.siguiente == None):
             return nodo2.data
         else:
             self.retornarUltimoNodo(self.Nodo.siguiente)
+    
+
+    #DEVUELVE EL TAMAÑO DE LA LISTA EN O(1)
     def retornarTamano(self):
         return self.tam
+    
+
+    #DEVUELVE EL NODO EN UN DETERMINADO INDICE
     def devolverNodo(self, posicion):
         nodo = self.Nodo
         i = 0
